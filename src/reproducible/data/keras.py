ed1176ff54be1d35c3d89e4a27f1d3da5b4755fc
Lachlan Gunn <lachlan.gunn@unisa.edu.au>
2017-09-04 16:48:28 +0930

    Fix data.keras.ModelData.cache_id

--- src/reproducible/data/keras.py
+++ src/reproducible/data/keras.py
@@ -14,9 +14,8 @@ class ModelData(reproducible.generic.Data):
         hash_context = reproducible.hash_family()
         hash_context.update(type(self.model).__name__.encode('utf8'))
         hash_context.update(self.model.to_json().encode('utf8'))
-        hash_context.update(numpy.array2string(self.model.get_weights()))
+        hash_context.update(';'.join([numpy.array2string(weight) for weight in self.model.get_weights()]).encode('utf8'))
         return str(base64.b16encode(hash_context.digest()),
                    'ascii').lower()
-
 
 reproducible.register_type(keras.models.Model, ModelData)
