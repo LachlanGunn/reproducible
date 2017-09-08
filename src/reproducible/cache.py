e25aad1abf0fa8eebd0bba94aef2be83b80378d2
Lachlan Gunn <lachlan.gunn@unisa.edu.au>
2017-09-08 12:58:26 +0930

    Add debug option to FileCache.

--- src/reproducible/cache.py
+++ src/reproducible/cache.py
@@ -5,6 +5,7 @@ import pickle
 
 import reproducible
 import reproducible.data
+import sys
 
 
 class Cache(object):
@@ -38,23 +39,34 @@ class FileCache(Cache):
     def __check_directory__(cls, root: str) -> bool:
         return os.path.isdir(root)
 
-    def __init__(self, root: str):
+    def __init__(self, root: str, debug: bool=False):
         super().__init__()
         if not self.__check_directory__(root):
             os.mkdir(root)
         self.root = root
+        self.debug = debug
 
     def is_cached(self, key: str) -> bool:
         return self.__check_directory__(os.path.join(self.root, key))
 
     def get(self, key: str) -> object:
+        if self.debug:
+            print("GET %s\n -> " % (key), file=sys.stderr, end="")
         base_path = os.path.join(self.root, key)
         with open(os.path.join(base_path, 'data'), 'rb') as fh, \
              open(os.path.join(base_path, 'type'), 'rb') as fh_type:
             data_type = pickle.load(fh_type)
+            if self.debug:
+                data = fh.read()
+                hash_context = reproducible.hash_family()
+                hash_context.update(data)
+                print(hash_context.hexdigest(), file=sys.stderr)
+                return data_type.loads(data)
             return data_type.load(fh)
 
     def set(self, key: str, value: reproducible.data.Data):
+        if self.debug:
+            print('SET %s' % key, file=sys.stderr)
         base_path = os.path.join(self.root, key)
         if not self.__check_directory__(base_path):
             os.mkdir(base_path)
