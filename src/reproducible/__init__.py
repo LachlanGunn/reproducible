13fec51a6a9a4ac997697d672260a97650d623f3
Lachlan Gunn <lachlan.gunn@unisa.edu.au>
2017-09-12 13:59:05 +0930

    Make module docstring more legible.

--- src/reproducible/__init__.py
+++ src/reproducible/__init__.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+"""Reproducible scientific computing tools for Python
+
+The reproducible module for Python provides tools to aid in performing
+reproducible scientific computation.
+
+By supporting a more functional style of programming, we allow parameters
+to be described at the top level of a computation. This will allow us to
+make full use of the literate programming capabilities provided by
+PythonTex without either a long wait every time we compile or external
+result caches that must be kept in sync.
+"""
 
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
@@ -11,4 +22,4 @@ from .wrapper import *
 
 hash_family = hashlib.sha256
 
-__all__ = [hash_family, Data, FileData]
+__all__ = ['operation', 'cache_ignore']
